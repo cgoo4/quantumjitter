95b2b3c6c3b4fde496fe3c8948188b951cad7158
Carl Goodwin <carl.goodwin@quantumjitter.com>
2023-12-22 18:08:41 +0000

    Project Wood updated to use the new trelliscope package

--- project/wood/index.R
+++ project/wood/index.R
@@ -1,8 +1,7 @@
 library(conflicted)
 library(tidyverse)
 conflict_prefer_all("dplyr", quiet = TRUE)
-library(trelliscopejs)
-library(rbokeh)
+library(trelliscope)
 library(janitor)
 library(vangogh)
 library(usedthese)
@@ -13,44 +12,29 @@ theme_set(theme_bw())
 
 (cols <- vangogh_palette("StarryNight"))
 
-cols9 <- colorRampPalette(cols)(9)
+cols12 <- colorRampPalette(cols)(12)
 
-url <- str_c(
-  "https://data.london.gov.uk/",
-  "download/recorded_crime_rates/",
-  "c051c7ec-c3ad-4534-bbfe-6bdfee2ef6bb/",
-  "crime%20rates.csv"
-)
-
-crime_df <-
-  read_csv(url, col_types = "cfcfdn") |>
-  clean_names() |>
-  mutate(
-    year = str_extract(year, "(?:1999|200[0-9]|201[0-7])"),
-    year = as.numeric(year)
-  ) |>
-  summarise(number_of_offences = sum(number_of_offences),
-            .by = c(year, borough, offences)) |>
-  filter(
-    offences != "All recorded offences",
-    !borough %in% c(
-      "England and Wales",
-      "Met Police Area",
-      "Inner London",
-      "Outer London"
-    )
-  )
+crime_df <- str_c(
+  "https://data.london.gov.uk/download/recorded_crime_summary/",
+  "934f2ddb-5804-4c6a-a17c-bdd79b33430e/", 
+  "MPS%20Borough%20Level%20Crime%20%28Historical%29.csv"
+  ) |> 
+  read_csv(show_col_types = FALSE) |> 
+  clean_names() |> 
+  rename_with(\(x) str_remove_all(x, "_text|look_up_|_name")) |> 
+  pivot_longer(where(is.numeric), names_to = "month", values_to = "num_offences") |> 
+  mutate(month = parse_number(month) |> str_c("01") |> ymd())
 
 crime_df |>
-  mutate(borough = str_wrap(borough, 11)) |>
-  ggplot(aes(year, number_of_offences, colour = offences, group = offences)) +
+  summarise(num_offences = sum(num_offences), .by = c(major, borough, month)) |>
+  ggplot(aes(month, num_offences, colour = major, group = major)) +
   geom_line() +
   facet_wrap(~borough, scales = "free_y", ncol = 4) +
   labs(
     x = NULL, y = NULL, title = "London Crime by Borough",
     colour = "Offence", caption = "Source: data.gov.uk"
   ) +
-  scale_colour_manual(values = cols9) +
+  scale_colour_manual(values = cols12) +
   guides(colour = guide_legend(nrow = 3)) +
   theme(
     strip.background = element_rect(fill = cols[4]),
@@ -58,54 +42,43 @@ crime_df |>
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
 
+panels_df <- crime_df |>
+  mutate(major = str_wrap(major, 16)) |> 
+  ggplot(aes(month, num_offences)) +
+  geom_line(show.legend = FALSE) +
+  geom_smooth(method = "lm", se = FALSE, colour = cols[5]) +
+  facet_panels(vars(borough, major, minor), scales = "free") + 
+  labs(colour = NULL, x = NULL, y = "Offence Count")
+
 slope <- \(x, y) coef(lm(y ~ x))[2]
 
-plot_data <- crime_df |>
-  nest(.by = c(borough, offences)) |>
-  mutate(
-    additional_cogs = map_cog(
-      data,
-      ~ tibble(
-        slope = cog(slope(.x$year, .x$number_of_offences),
-          desc = "Steepness of the trend"
-        ) |>
-          round(2),
-        mean_count = cog(mean(.x$number_of_offences),
-          desc = "Average count"
-        ),
-        iqr_count = cog(IQR(.x$number_of_offences),
-          desc = "Interquartile range"
-        )
+summary_df <- crime_df |> 
+  summarise(
+    mean_count = mean(num_offences), 
+    slope = slope(month, num_offences),
+    .by = c(borough, major, minor))
+  
+panels_df |> 
+  as_panels_df(as_plotly = TRUE) |> 
+  as_trelliscope_df(
+    name = "Crime in 'The Smoke'",
+    description = str_c(
+      "Timeseries of offences by category ", 
+      "across London's 33 boroughs sourced from data.gov.uk."
       )
-    ),
-    panel = map_plot(
-      data,
-      ~ figure(xlab = "Date", ylab = "Count") |>
-        ly_lines(year, number_of_offences, color = cols[5], 
-                 width = 2, data = .x) |>
-        ly_points(year, number_of_offences,
-          size = 10,
-          fill_color = cols[9],
-          hover = number_of_offences, data = .x
-        ) |>
-        theme_plot(
-          background_fill_color = cols[2],
-          background_fill_alpha = 0.5
-        )
-    )
-  )
-
-plot_data |>
-  trelliscope(
-    name = "London Crime",
-    desc = "Source: data.gov.uk",
-    nrow = 2,
-    ncol = 3,
-    state = list(
-      sort = list(sort_spec("slope", dir = "desc")),
-      labels = c("borough", "offences", "slope")
-    ),
-    path = "appfiles"
-  )
+    ) |> 
+  left_join(summary_df, join_by(borough, major, minor)) |> 
+  set_var_labels(
+    major = "Major Category of Offence",
+    minor = "Minor Category of Offence",
+    mean_count = "Average Offences by Borough & Offence Category",
+    slope = "Steepness of a Linear Trendline"
+  ) |> 
+  set_default_sort(c("slope"), dirs = "desc") |>
+  set_tags(
+    stats = c("mean_count", "slope"),
+    info = c("borough", "major", "minor")
+  ) |> 
+  view_trelliscope()
 
 used_here()
