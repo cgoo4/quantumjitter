f3ce7eea64ea40e7bd660c62fd52c26e80d76d27
Carl Goodwin <carl.goodwin@quantumjitter.com>
2024-06-23 12:39:15 +0100

    Use ggfoundry & paletteer in hansard

--- project/hansard/index.R
+++ project/hansard/index.R
@@ -2,7 +2,6 @@ library(conflicted)
 library(tidyverse)
 conflict_prefer_all("dplyr", quiet = TRUE)
 library(clock)
-library(wesanderson)
 library(hansard)
 library(dendextend)
 library(corrplot)
@@ -11,13 +10,20 @@ library(factoextra)
 library(glue)
 library(ggrepel)
 library(geomtextpath)
+library(ggfoundry)
+library(paletteer)
 library(usedthese)
 
 conflict_scout()
 
 theme_set(theme_bw())
 
-(cols <- wes_palette(name = "Moonrise2"))
+n <- 4
+pal_name <- "wesanderson::Moonrise2"
+
+pal <- paletteer_d(pal_name, n = n)
+
+display_palette(fill = pal, n = n, pal_name = pal_name)
 
 url_prefix <- "http://data.parliament.uk/members/"
 
@@ -89,9 +95,9 @@ scaled_df |>
   fviz_dist(
     show_labels = FALSE,
     gradient = list(
-      low = cols[1],
-      mid = cols[3],
-      high = cols[4]
+      low = pal[1],
+      mid = pal[3],
+      high = pal[4]
     )
   )
 
@@ -125,7 +131,7 @@ dend_list |>
   corrplot(
     "pie",
     "lower",
-    col = cols[1],
+    col = pal[1],
     mar = c(1, 0.5, 4, 0.5),
     order = "AOE",
     tl.cex = 0.8,
@@ -158,7 +164,7 @@ best_method <- map(methods, \(x) {
 
 best_method |>
   ggplot(aes(correlation, reorder(method, correlation))) +
-  geom_col(fill = cols[1], width = 0.8) +
+  geom_col(fill = pal[1], width = 0.8) +
   geom_text(aes(label = str_c(method, "  ", round(correlation, 2))),
     hjust = 1.3, colour = "white"
   ) +
@@ -175,8 +181,8 @@ dend_avg <- orig_dist |>
 labels(dend_avg) <- scaled_df$mp[order.dendrogram(dend_avg)]
 
 dend <- dend_avg |>
-  color_branches(k = 2, col = cols[c(2, 1)]) |>
-  color_labels(k = 2, col = cols[c(2, 1)]) |>
+  color_branches(k = 2, col = pal[c(2, 1)]) |>
+  color_labels(k = 2, col = pal[c(2, 1)]) |>
   set("branches_lwd", 0.5) |> 
   set("labels_cex", 0.3)
 
@@ -191,10 +197,10 @@ p
 dend_cuts <- dend |>
   assign_values_to_leaves_nodePar(19, "pch") |>
   assign_values_to_leaves_nodePar(5, "cex") |>
-  assign_values_to_leaves_nodePar(cols[1], "col") |>
+  assign_values_to_leaves_nodePar(pal[1], "col") |>
   set("labels_cex", 0.4) |>
   set("branches_lwd", 2.5) |>
-  color_branches(k = 2, col = cols[1]) |>
+  color_branches(k = 2, col = pal[1]) |>
   cut(h = 50)
 
 start_formatted <- date_parse(start_date, format = "%Y-%m-%d") |> 
@@ -205,8 +211,8 @@ end_formatted <- date_parse(end_date, format = "%Y-%m-%d") |>
 
 ggplot(rev(dend_cuts$lower[[1]]),
   horiz = TRUE,
-  nodePar = nodePar,
-  offset_labels = -0.5
+  nodes = FALSE,
+  offset_labels = -0.6
 ) +
   labs(
     y = "\nDistance", 
@@ -231,9 +237,9 @@ fewest_votes <- votes |>
 
 fewest_votes |>
   ggplot(aes(value, mp, fill = name)) +
-  geom_col() +
+  geom_col(show.legend = FALSE) +
   geom_label(aes(label = value), position = position_stack()) +
-  scale_fill_manual(values = cols[c(1, 3)]) +
+  scale_fill_manual(values = pal[c(1, 3)]) +
   labs(title = "Labour MPs Voting Fewest Times",
        y = "Votes", x = NULL, fill = NULL)
 
@@ -248,7 +254,7 @@ mod_df <- mod |>
 
 mod_df |>
   mutate(label = str_c(mp, "\n", str_wrap(title, 25))) |>
-  ggplot(aes(.cooksd, title, colour = mp)) +
+  ggplot(aes(.cooksd, title, colour = if_else(.cooksd > 0.002, mp, NA))) +
   geom_jitter(size = 0.4) +
   geom_label_repel(aes(label = if_else(.cooksd > 0.002, label, NA)), size = 1.5) +
   geom_texthline(
@@ -257,9 +263,9 @@ mod_df |>
       angle = 30, length = unit(0.1, "inches"),
       ends = "last", type = "closed"
     ),
-    hjust = 0, vjust = 0.5, color = cols[1]
+    hjust = 0, vjust = 0.5, color = pal[1]
   ) +
-  scale_colour_manual(values = wes_palette(220, name = "Moonrise2", type = "continuous")) +
+  scale_colour_manual(values = pal[c(1:4, 1)]) +
   labs(x = NULL, y = NULL) +
   theme(
     panel.border = element_blank(),
@@ -272,17 +278,16 @@ mod_df |>
 mod_df |>
   filter(str_detect(title, "759161|824379|809989")) |>
   mutate(title = str_wrap(title, 30)) |> 
-  ggplot(aes(.cooksd, title, colour = mp)) +
+  ggplot(aes(.cooksd, title, colour = if_else(.cooksd > 0.0015, mp, NA))) +
   geom_point(size = 4) +
   geom_label_repel(aes(label = if_else(.cooksd > 0.0015, mp, NA)), size = 4) +
   ggtitle("Cook's Distance") +
   theme(
-    axis.line.x = element_line(color = "grey60"),
+    axis.line.x = element_line(colour = "grey60"),
     axis.text = element_text(size = 8),
     legend.position = "none",
     axis.title = element_blank()
   ) +
-  scale_colour_manual(values = wes_palette(
-    210, name = "Moonrise2", type = "continuous"))
+  scale_colour_manual(values = pal[c(1:4, 1:2)])
 
 used_here()
