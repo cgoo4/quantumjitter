d9fe091fe1ac5f7fee47f189f1a9cdec9414343c
Carl Goodwin <carl.goodwin@quantumjitter.com>
2026-09-25 15:33:35 +0100

    Refactor SW10

--- project/sw10/index.R
+++ project/sw10/index.R
@@ -1,11 +1,8 @@
 library(conflicted)
 library(tidyverse)
-conflict_prefer_all("dplyr", quiet = TRUE)
-conflict_prefer("as_date", "lubridate")
+library(sparqlr)
 library(scales)
-library(SPARQL)
 library(clock)
-conflict_prefer("date_format", "clock")
 library(glue)
 library(tsibble)
 library(patchwork)
@@ -13,6 +10,9 @@ library(ggmosaic)
 library(ggfoundry)
 library(paletteer)
 library(usedthese)
+
+conflict_prefer_all("dplyr", quiet = TRUE)
+conflicts_prefer(lubridate::as_date, clock::date_format)
 
 conflict_scout()
 
@@ -61,128 +61,175 @@ WHERE
   OPTIONAL { ?item ppd:recordStatus ?ppd_recordStatus }
 }'
 
-data_lst <- SPARQL(endpoint, query)
+data_df <- sparql_select(endpoint, query)
 
-data_df <- data_lst$results |>
-  as_tibble() |>
+data_df <- data_df |>
   mutate(
-    trans_date = as_datetime(ppd_transactionDate) |> as_date(),
+    trans_date = as_date(ppd_transactionDate),
     amount = ppd_pricePaid,
     prop_type = str_extract(ppd_propertyType, "(?<=common/)[\\w]+"),
     est_type = str_extract(ppd_estateType, "(?<=common/)[\\w]+"),
-    cat = str_remove(ppd_transactionCategory, "<http://landregistry.data.gov.uk/def/ppi/"),
-    prop_type = recode(prop_type, otherPropertyType = "Other")
+    cat = str_remove(
+      ppd_transactionCategory,
+      "<http://landregistry.data.gov.uk/def/ppi/"
+    ),
+    prop_type = replace_values(prop_type, "otherPropertyType" ~ "Other")
   ) |>
   filter(str_detect(cat, "standard"))
 
 events <- tribble(
-  ~event_date, ~change,
-  "96-07-31", "Stamp Duty £250k (1.5%) £500k (2%)",
-  "98-03-31", "£250k (2%) £500k (3%)",
-  "99-03-31", "£250k (2.5%) £500k (3.5%)",
-  "00-03-31", "£250k (3%) £500k (4%)",
-  "11-04-30", "£250k (3%) £500k (4%) £1m (5%)",
-  "12-03-31", "£250k (3%) £500k (4%) £1m (5%) £2m (7%)",
-  "14-12-31", "£250k (5%) £925k (10%) 1.5m (12%)",
-  "07-08-09", "Financial Crisis",
-  "16-06-23", "Brexit Vote",
-  "20-03-23", "Covid-19 Lockdown"
+  ~event_date , ~change                                   ,
+  "96-07-31"  , "Stamp Duty £250k (1.5%) £500k (2%)"      ,
+  "98-03-31"  , "£250k (2%) £500k (3%)"                   ,
+  "99-03-31"  , "£250k (2.5%) £500k (3.5%)"               ,
+  "00-03-31"  , "£250k (3%) £500k (4%)"                   ,
+  "11-04-30"  , "£250k (3%) £500k (4%) £1m (5%)"          ,
+  "12-03-31"  , "£250k (3%) £500k (4%) £1m (5%) £2m (7%)" ,
+  "14-12-31"  , "£250k (5%) £925k (10%) 1.5m (12%)"       ,
+  "07-08-09"  , "Financial Crisis"                        ,
+  "16-06-23"  , "Brexit Vote"                             ,
+  "20-03-23"  , "Covid-19 Lockdown"
 ) |>
   mutate(event_date = date_parse(event_date, format = "%y-%m-%d"))
 
-events |> 
+events |>
   rename("Date" = event_date, "Event" = change)
 
-to_date <- data_df |> 
-  summarise(max(trans_date)) |> 
-  pull() |> 
+to_date <- data_df |>
+  summarise(max(trans_date)) |>
+  pull() |>
   date_format(format = "%b %d, %Y")
 
 data_df |>
   ggplot(aes(trans_date, amount, colour = est_type)) +
   geom_point(alpha = 0.2, size = 0.7, show.legend = FALSE) +
-  geom_smooth(se = FALSE, aes(linetype = est_type), size = 1.2) +
+  geom_smooth(
+    se = FALSE,
+    aes(linetype = est_type),
+    method = "gam",
+    formula = y ~ s(x, bs = "cs"),
+    linewidth = 1.2
+  ) +
   labs(
     title = "SW10 Standard House Prices",
-    subtitle = glue("Prices Paid to {to_date} (Prices > £5m Not Shown)"
-    ),
+    subtitle = glue("Prices Paid to {to_date} (Prices > £5m Not Shown)"),
     x = NULL,
     y = NULL,
-    colour = "Type", linetype = "Type",
+    colour = "Type",
+    linetype = "Type",
     caption = "Source: HM Land Registry"
   ) +
-  geom_vline(xintercept = events$event_date, 
-             size = 0.5, lty = 2, alpha = 0.4) +
-  annotate("text", events$event_date, 5000000,
+  geom_vline(
+    xintercept = events$event_date,
+    linewidth = 0.5,
+    lty = 2,
+    alpha = 0.4
+  ) +
+  annotate(
+    "text",
+    events$event_date,
+    5000000,
     angle = 90,
-    label = events$change, vjust = 1.4, hjust = 1, size = 3, fontface = 2
+    label = events$change,
+    vjust = 1.4,
+    hjust = 1,
+    size = 3,
+    fontface = 2
   ) +
   coord_cartesian(ylim = c(0, 5000000)) +
   scale_colour_manual(values = pal[c(2, 3)]) +
   scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
-  scale_y_continuous(labels = label_currency(accuracy = 0.1, prefix = "£", 
-                                           scale_cut = cut_short_scale()))
+  scale_y_continuous(
+    labels = label_currency(
+      accuracy = 0.1,
+      prefix = "£",
+      scale_cut = cut_short_scale()
+    )
+  )
 
-qtr_start <- date_today("Europe/London") |> 
-  as_year_quarter_day() |> 
-  calendar_start("quarter") |> 
+qtr_start <- date_today("Europe/London") |>
+  as_year_quarter_day() |>
+  calendar_start("quarter") |>
   as_date()
 
-qtile_df <- 
-  data_df |> 
-  filter(trans_date < qtr_start) |> 
-  mutate(yr_qtr = yearquarter(trans_date)) |> 
-  reframe(price = quantile(amount, c(0.25, 0.5, 0.75)), 
-            quantile = c("lower", "median", "upper") |> factor(),
-            n = n(),
-            .by = yr_qtr) |> 
+qtile_df <-
+  data_df |>
+  filter(trans_date < qtr_start) |>
+  mutate(yr_qtr = yearquarter(trans_date)) |>
+  reframe(
+    price = quantile(amount, c(0.25, 0.5, 0.75)),
+    quantile = c("lower", "median", "upper") |> factor(),
+    n = n(),
+    .by = yr_qtr
+  ) |>
   pivot_wider(names_from = quantile, values_from = price)
 
 last <- qtile_df |> summarise(max(yr_qtr)) |> pull()
 first <- qtile_df |> summarise(min(yr_qtr)) |> pull()
 
-p1 <- qtile_df |> 
+p1 <- qtile_df |>
   ggplot(aes(yr_qtr, median)) +
   geom_ribbon(aes(ymin = lower, ymax = upper), fill = pal[5]) +
   geom_line(colour = "white") +
   geom_hline(yintercept = 1000000, linetype = "dashed") +
-  annotate("text", x = yearquarter("2020 Q2"), y = 300000, 
-           label = "Covid-19\nLockdown", size = 3) +
+  annotate(
+    "text",
+    x = yearquarter("2020 Q2"),
+    y = 300000,
+    label = "Covid-19\nLockdown",
+    size = 3
+  ) +
   scale_x_yearquarter(date_breaks = "2 years") +
-  scale_y_log10(labels = label_currency(prefix = "£", 
-                                      scale_cut = cut_short_scale())) +
+  scale_y_log10(
+    labels = label_currency(prefix = "£", scale_cut = cut_short_scale())
+  ) +
   labs(
-    title = glue("Median Quarterly SW10 Property Prices ({first} to {last})"), 
+    title = glue("Median Quarterly SW10 Property Prices ({first} to {last})"),
     subtitle = "With Upper / Lower Price Quartiles & Sales Volume",
-    x = NULL, y = "Price (Log10)"
-    ) +
+    x = NULL,
+    y = "Price (Log10)"
+  ) +
   theme(axis.text.x = element_blank())
 
-p2 <- qtile_df |> 
+p2 <- qtile_df |>
   ggplot(aes(yr_qtr, n)) +
   geom_line() +
-  annotate("text", x = yearquarter("2007 Q3"), y = 180, 
-           label = "Financial\nCrisis", size = 3) +
-  annotate("text", x = yearquarter("2016 Q3"), y = 130, 
-           label = "Brexit\nVote", size = 3) +
+  annotate(
+    "text",
+    x = yearquarter("2007 Q3"),
+    y = 180,
+    label = "Financial\nCrisis",
+    size = 3
+  ) +
+  annotate(
+    "text",
+    x = yearquarter("2016 Q3"),
+    y = 130,
+    label = "Brexit\nVote",
+    size = 3
+  ) +
   scale_x_yearquarter(date_breaks = "2 years") +
-  labs(x = NULL, y = "Transactions",
-       caption = "Source: HM Land Registry") +
+  labs(x = NULL, y = "Transactions", caption = "Source: HM Land Registry") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 p1 / p2 + plot_layout(heights = c(2, 1))
 
 num_trans <- data_df |> nrow()
 
-data_df |> 
+data_df |>
   ggplot() +
-  geom_mosaic(aes(product(prop_type, est_type), fill = prop_type), 
-              offset = 0.02, divider = mosaic("h")) +
-  scale_fill_manual(values = pal[c(2:5)]) + 
+  geom_mosaic(
+    aes(product(prop_type, est_type), fill = prop_type),
+    offset = 0.02,
+    divider = mosaic("h")
+  ) +
+  scale_fill_manual(values = pal[c(2:5)]) +
   labs(
     title = "SW10 Transactions by Estate & Property Types",
     subtitle = glue("{comma(num_trans)} Transactions to {to_date}"),
-    x = "", y = "", fill = "Property Type", 
+    x = "",
+    y = "",
+    fill = "Property Type",
     caption = "Source: HM Land Registry"
   ) +
   theme_minimal()
